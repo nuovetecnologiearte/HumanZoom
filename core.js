a3e896a95b987e284f542c6c26bf5e7e2b2ffae7
Nuove Tecnologie Dell'Arte <59918869+nuovetecnologiearte@users.noreply.github.com>
2020-05-15 12:12:14 +0200

    Update core.js

--- core.js
+++ core.js
@@ -1,46 +1,45 @@
-var count = 0;
-var arrayLength = 0;
-function prelevaImg(){
+ var count = 0;
+ var arrayImmagini = [];
+ function prelevaImg(){
     count = 0;
-    var param = document.getElementById('your_name').value;
-    var url = 'https://www.instagram.com/explore/tags/'+param+'/?__a=1&callback?';
-    document.getElementById('hideAfterClick').style.display='none';
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
+     var param ='photooftheday'; //document.getElementById('your_name').value;
+    var url = 'http://www.instagram.com/explore/tags/'+param+'/?__a=1&callback?';
+    //  document.getElementById('hideAfterClick').style.display='none';
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
 
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-      // Success!
-        var data = JSON.parse(request.responseText);
-        console.log(data);
-        arrayLength=data.graphql.hashtag.edge_hashtag_to_media.edges.length;
-        for (let i=0; i<data.graphql.hashtag.edge_hashtag_to_media.edges.length; i++ ){
-          var $this = data.graphql.hashtag.edge_hashtag_to_media.edges[i].node;
-          document.getElementById('images').innerHTML += '<img src="'+$this.display_url+'">';
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+        // Success!
+          var data = JSON.parse(request.responseText);
+          console.log(data);
+          arrayImmagini = data.graphql.hashtag.edge_hashtag_to_media.edges;
+          setTimeout(updateDiv, 500)
+
+        } else {
+          // We reached our target server, but it returned an error
+          console.log('errore nel prelevare il json');
+          setTimeout(prelevaImg, 5000);
         }
+      };
 
-        setTimeout(deleteDiv, 5000)
-
-      } else {
-        // We reached our target server, but it returned an error
-        console.log('errore nel prelevare il json');
-        setTimeout(prelevaImg, 5000);
-      }
-    };
-
-    request.onerror = function() {
-      // There was a connection error of some sort
-      console.log('il server non risponde');
-    };
-      request.send();
-}
-
-function deleteDiv(){
-  document.getElementsByTagName('img')[0].remove();
-  if(count>arrayLength-5){
-    prelevaImg();
-  } else{
-    count++;
-    setTimeout(deleteDiv, 500);
+      request.onerror = function() {
+        // There was a connection error of some sort
+        console.log('il server non risponde');
+      };
+        request.send();
   }
-}
+
+  function updateDiv(){
+    if (count < arrayImmagini.length ){
+      document.getElementById('image' + Math.floor(Math.random()*9)).innerHTML='<img src="'+arrayImmagini[count].node.display_url+'">';
+      count++;
+      setTimeout(updateDiv, 1000);
+    }
+    else {
+      count=0;
+      setTimeout(prelevaImg, 1000);
+    }
+      
+     
+  } 
